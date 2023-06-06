fce9109399aee18eecefa44bc2bc7699e0cdd414
liyyao <2813789717@qq.com>
2023-06-06 16:16:11 +0800

    Initial commit

---
+++ transfer-service/src/test/java/com/liyyao/transfer/web/TransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liyyao.transfer.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
